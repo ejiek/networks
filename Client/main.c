2be14bd18e355f4315a6c370e4c43c81065c6937
Vlad P <ejiek@mail.ru>
2015-09-30 18:37:03 +0300

    smth done init

---
+++ Client/main.c
@@ -0,0 +1,73 @@
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+ 
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char message[1000] , server_reply[2000];
+     
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+ 
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+     
+    //keep communicating with server
+    while(1)
+    {
+        printf("Enter message : ");
+        scanf("%s" , message);
+	printf("%d", sizeof(message));
+	if(message!="quit"){
+		//Send some data
+       		if( send(sock , message , strlen(message) , 0) < 0)
+        	{
+            		puts("Send failed");
+            		return 1;
+        	}
+         
+        	//Receive a reply from the server
+        	if( recv(sock , server_reply , 2000 , 0) < 0)
+        	{
+        	    	puts("recv failed");
+	            	break;
+        	}
+         
+        	puts("Server reply :");
+        	puts(server_reply);
+    	}
+	else{
+		break;
+		puts("win!!!!!");
+	}
+    } 
+    if(shutdown(sock, SHUT_RDWR) == 0){    
+    	if(close(sock) == 0){
+		puts("win");
+	}
+	else{
+		puts("you alac");
+		return 1;
+	}
+    }
+    else puts("you flac");
+    return 0;
+}
---
+++ Server/main.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h> 
+#include<unistd.h>    
+ 
+int main(int argc , char *argv[])
+{
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[2000];
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1){
+        puts("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0){
+        //print the error message
+        puts("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0){
+        puts("Fail: accept");
+        return 1;
+    }
+    puts("Connection accepted");
+     
+    //Receive a message from client
+    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 ){
+        //Send the message back to client
+        write(client_sock , client_message , strlen(client_message));
+    }
+     
+    if(read_size == 0){
+        puts("Client disconnected");
+	if(shutdown(socket_desc, SHUT_RDWR) == 0){
+		puts("Socket is down");
+	    	if(close(socket_desc) == 0){
+			puts("Socket is closed");
+		}
+		else{
+			puts("Fail: cannot close socket");
+			return 1;
+		}
+	}
+	else{
+		 puts("Fail: cannot shut socket down");
+    		return 1;
+   	}
+    }
+    else if(read_size == -1){
+        puts("recv failed");
+	return 1;
+    }
+     
+    return 0;
+}
