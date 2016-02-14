1bcce6d64b2bb40142fac64129bc2297c96acc59
Vlad Petrov <ejiek@mail.ru>
2016-02-14 20:36:00 +0300

    fixed: buffering

--- UDP/server.c
+++ UDP/server.c
@@ -60,11 +60,11 @@ int add_po_news(char *);
 int show_news(char * , char *);
 void help(char *);
 
-int mn_recv(int , char *, int *, struct mes_buf *[100]);
+int mn_recv(int , char *, int *, struct mes_buf [100]);
 int get_mn(char msg[BUFLEN]);
-int add_to_buf(char *, struct mes_buf *mesbuf[100]);
+int add_to_buf(char *, struct mes_buf mesbuf[100]);
 int nprint(int, char *);
-int get_from_buf(int , struct mes_buf *mesbuf[100], char *);
+int get_from_buf(int , struct mes_buf mesbuf[100], char *);
 
 int main(int argc , char *argv[])
 {
@@ -453,14 +453,15 @@ void help(char *reply){
     strcpy(reply, "Available commands:\nLIST - prints a list of themes\nLIST [theme id] - prints a list of themes from the theme\nADDN [theme id] [text] - adds piece of news to the theme\nHELP - prints this help\nSHOW [id] - prints the piece of news\n");
 }
 
-int mn_recv(int sock, char *client_message, int *mn, struct mes_buf *mesbuf[100]){
+int mn_recv(int sock, char *client_message, int *mn, struct mes_buf mesbuf[100]){
     char msg_with_n[BUFLEN], tmp[BUFLEN];
     int read_size, new_mn;
     if( (read_size = recv(sock , msg_with_n, BUFLEN , 0)) >= 0){
         new_mn = get_mn(msg_with_n);
         printf("recieved number: %d\n", new_mn);
-        if( get_from_buf(*mn+1, mesbuf[100], tmp) == 0){
+        if( get_from_buf(*mn+1, &mesbuf[100], tmp) == 0){
             *mn = *mn + 1;
+            puts("da");
         }
         else{
             strcpy(tmp, msg_with_n+4);
@@ -472,7 +473,7 @@ int mn_recv(int sock, char *client_message, int *mn, struct mes_buf *mesbuf[100]
             }
             else{
                 if(new_mn > *mn){
-                    add_to_buf(tmp, mesbuf[100]);
+                    add_to_buf(tmp, &mesbuf[100]);
                 }
             }
         }
@@ -485,22 +486,23 @@ int get_mn(char msg[BUFLEN]){
     return strtoul(msg, NULL,10);
 }
 
-int add_to_buf(char *msg, struct mes_buf *mesbuf[100]){
+int add_to_buf(char *msg, struct mes_buf mesbuf[100]){
   for(int i; i < 100; i++){
-    if(mesbuf[i]->msg[0] == '\0'){
-        strncpy(mesbuf[i]->msg, msg, BUFLEN);
+    if(mesbuf[i].msg[0] == '\0'){
+        strncpy(mesbuf[i].msg, msg, BUFLEN);
         break;
     }
   }
 }
 
-int get_from_buf(int nm, struct mes_buf *mesbuf[100], char *tmp){
+int get_from_buf(int nm, struct mes_buf mesbuf[100], char *tmp){
     char n[3];
     nprint(nm, n);
+    printf("number: %d buffer: %s\n", nm, n);
     for(int i; i < 100; i++){
-        if(strncmp(mesbuf[i]->msg, n, 3) == 0){
-            strcpy(tmp, mesbuf[i]->msg+4);
-            bzero(mesbuf[i]->msg, BUFLEN);
+        if(strncmp(mesbuf[i].msg, n, 3) == 0){
+            strcpy(tmp, mesbuf[i].msg+4);
+            bzero(mesbuf[i].msg, BUFLEN);
             return 0;
         }
     }
@@ -509,6 +511,7 @@ int get_from_buf(int nm, struct mes_buf *mesbuf[100], char *tmp){
 
 int nprint(int n, char *message){
   if (n < 0) return -1;
+  strncpy(message, "000 ", 4);
   if(n < 10) sprintf(message+2, "%d", n);
   else if(n < 100) sprintf(message+1, "%d", n);
   else if(n < 1000) sprintf(message, "%d", n);
