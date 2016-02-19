b492c5236dfd5cad051547b6d27b0de2db83581a
Vlad Petrov <ejiek@mail.ru>
2016-02-20 02:46:31 +0300

    added: more libs to enet.h

--- UDP/enet.h
+++ UDP/enet.h
@@ -1,7 +1,11 @@
 #ifndef ENET_H_
 #define ENET_H_
 
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/select.h>
 #include <sys/time.h>
